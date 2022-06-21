454c44ece842c91a6c0fa45228ac5c2ac2016a3e
xSjnxyz <mailme.suzan77@gmail.com>
2022-06-21 09:18:33 +0545

    Add: validation pipes. + fix: massive bug.

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,12 +1,16 @@
-import { IsEmail } from 'class-validator';
+import { IsEmail, IsString } from 'class-validator';
 
 export class AuthRegistrationDto {
+  @IsString()
   username: string;
+  @IsString()
   password: string;
   @IsEmail()
   email: string;
 }
 export class AuthLoginDto {
+  @IsString()
   username: string;
+  @IsString()
   password: string;
 }
