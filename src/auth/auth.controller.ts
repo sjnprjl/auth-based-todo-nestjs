2bff3b7a92e8f478eddf5047ae55fb7130799d1b
xSjnxyz <mailme.suzan77@gmail.com>
2022-06-21 16:33:39 +0545

    Feat: role based auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Delete,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { AuthLoginDto, AuthRegistrationDto } from './dto/auth.dto';
 import { AuthService } from './auth.service';
 import { Public } from '../shared/decorators/public.decorator';
+import { Roles } from 'src/shared/decorators/role.decorator';
+import { Role } from 'src/users/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -18,5 +27,11 @@ export class AuthController {
   @Post('register')
   register(@Body() body: AuthRegistrationDto) {
     return this.authService.register(body);
+  }
+
+  @Roles(Role.admin)
+  @Delete('delete/:id')
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.authService.delete(id);
   }
 }
