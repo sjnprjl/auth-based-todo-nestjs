69c0f1d74df5e999d04e2dfd3a0f6c9c3f79c093
xSjnxyz <mailme.suzan77@gmail.com>
2022-06-21 16:34:33 +0545

    fix: `validate()` func

--- src/auth/passports/jwt.strategy.ts
+++ src/auth/passports/jwt.strategy.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(
     private readonly userService: UsersService,
     private readonly configService: ConfigService,
@@ -17,6 +17,9 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
   async validate(payload: any) {
-    return { userId: payload.sub, username: payload.username };
+    const user = await this.userService.findOne({ id: payload.sub });
+    if (!user) throw new UnauthorizedException();
+    delete user.password;
+    return user;
   }
 }
