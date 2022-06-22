3a4e3596092fc1074141415c7950df6f444eab67
xSjnxyz <mailme.suzan77@gmail.com>
2022-06-22 11:09:20 +0545

    refactor:

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { TodoController } from './todo.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Todo } from './todo.entity';
+import { Todo } from './entities/todo.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Todo])],
